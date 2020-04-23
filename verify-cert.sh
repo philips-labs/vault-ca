66cc8170d981cd153ffffd300c3a30a9fe0ca056
Marco Franssen <marco.franssen@philips.com>
2020-04-23 13:52:52 +0200

    Enhance verify script to traverse the full certificate chain

--- verify-cert.sh
+++ verify-cert.sh
@@ -2,11 +2,26 @@
 
 cert=$1
 
-ca_url=$(openssl x509 -noout -text -in $cert | grep 'CA Issuers - URI' | cut -d ':' -f2-)
-crl_url=$(openssl x509 -noout -text -in $cert | grep -A 4 'X509v3 CRL Distribution Points' | grep URI: | cut -d ':' -f2-)
-ca=$(curl -s $ca_url/pem)
-crl=$(curl -s $crl_url/pem)
-echo -e "$crl\n$ca" > chain.pem
+function get_issuers_uri {
+  echo -e "$1" | openssl x509 -noout -text | grep 'CA Issuers - URI' | cut -d ':' -f2-
+}
+
+function get_crl_uri {
+  echo -e "$1" | openssl x509 -noout -text | grep -A 4 'X509v3 CRL Distribution Points' | grep URI: | cut -d ':' -f2-
+}
+
+function download {
+  curl -s $1/pem
+}
+
+ca_url=$(get_issuers_uri "$(cat $cert)")
+crl_url=$(get_crl_uri "$(cat $cert)")
+ca=$(download $ca_url)
+crl=$(download $crl_url)
+
+get_crl_uri "$ca"
+chain="$crl\n$(download $(get_crl_uri "$ca"))\n$ca\n$(download $(get_issuers_uri "$ca"))"
+echo -e "$chain" > chain.pem
 
 openssl verify -crl_check -CAfile chain.pem $cert
 
